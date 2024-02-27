55fef316a4aee0e4ba3146158bf7325b674b1e9e
Puskatel <159027319+Puskatel@users.noreply.github.com>
2024-02-27 15:26:39 +1000

    Add files via upload

---
+++ HW1/task2.c
@@ -0,0 +1,17 @@
+/*
+    HW1_task2
+Вывести на экран текст "лесенкой"
+     Let’s
+       go
+            to walk
+*/
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("    Let's\n");
+    printf("       go\n");
+    printf("         to walk");
+    return 0;
+}
+
---
+++ HW1/task3.c
@@ -0,0 +1,25 @@
+/*
+
+    HW1_task3
+Вывести на экран рисунок из букв
+     *
+    ***
+   *****
+  *******
+   HH HH
+   ZZZZZ
+*/
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("      *\n");
+    printf("     ***\n");
+    printf("    *****\n");
+    printf("   *******\n");
+    printf("    HH HH\n");
+    printf("    ZZZZZ");
+    return 0;
+}
+
